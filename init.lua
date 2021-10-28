b5389d9c773a8212d3fdf43ae382e60295865b4b
KokaKiwi <kokakiwi+git@kokakiwi.net>
2021-10-28 19:06:53 +0200

    Set a base colorscheme so that in case something goes wrong, neovim doesn't destroy my eyes with its default colorscheme.

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
 local ok, impatient = pcall(require, 'impatient')
 if ok then impatient.enable_profile() end
+
+-- Set a base colorscheme so that in case something goes wrong
+-- neovim doesn't destroy my eyes with its default colorscheme.
+vim.cmd [[colorscheme vimdark]]
+
 require('rc')
