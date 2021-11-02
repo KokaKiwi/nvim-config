98fd3cb8a378965c620501413d56c009af85defb
KokaKiwi <kokakiwi+git@kokakiwi.net>
2021-11-02 16:45:46 +0100

    Allow disabling lua caching when debugging

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
-local ok, impatient = pcall(require, 'impatient')
-if ok then impatient.enable_profile() end
+if os.getenv('NVIM_DEBUG') ~= '1' then
+  local ok, impatient = pcall(require, 'impatient')
+  if ok then impatient.enable_profile() end
+end
 
 -- Set a base colorscheme so that in case something goes wrong
 -- neovim doesn't destroy my eyes with its default colorscheme.
