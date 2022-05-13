108978ca984f9430cc9590633996d7cb6902f20f
KokaKiwi <kokakiwi+git@kokakiwi.net>
2022-05-13 21:50:10 +0200

    Remove custom LspRestart

--- pack/local/start/kiwi.nvim/lua/kiwi/lsp/init.lua
+++ pack/local/start/kiwi.nvim/lua/kiwi/lsp/init.lua
@@ -21,29 +21,6 @@ function lsp.setup()
     update_in_insert = false,
   })
 
-  vim.command('LspRestart', function()
-    local posix = require('posix')
-    local configs = require('lspconfig/configs')
-
-    vim.notify.info('Stopping LSP clients...')
-    local clients = vim.lsp.get_active_clients()
-    for _, client in ipairs(clients) do
-      client.stop()
-    end
-
-    posix.sleep(1)
-
-    vim.notify.info('Starting LSP clients...')
-    local buffer_filetype = vim.bo.filetype
-    for _, config in pairs(configs) do
-      for _, filetype_match in ipairs(config.filetypes or {}) do
-        if buffer_filetype == filetype_match then
-          config.autostart()
-        end
-      end
-    end
-  end)
-
   vim.command('LspFormat', function()
     vim.lsp.buf.format { async = true }
   end)
