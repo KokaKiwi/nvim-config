7a7dba601fef0a2f4865af2882b964f61f1bf24a
Julianne H <julianne.hervier@qonfucius.team>
2023-01-02 01:12:51 +0100

    Refactor local plugins

--- lua/rc/plugins/spec/local.lua
+++ lua/rc/plugins/spec/local.lua
@@ -3,18 +3,9 @@ local util = require('rc.plugins.util')
 local local_plugins_path = vim.fn.stdpath('config') .. '/local'
 
 return util.module('local', function(use)
-  local function use_local(name)
+  for _, entry in ipairs(fs.readdir(local_plugins_path)) do
     use {
-      dir = local_plugins_path .. '/' .. name,
+      dir = string.format('%s/%s', local_plugins_path, entry.name),
     }
   end
-
-  use_local 'kiwi.nvim'
-  use_local 'nkey.nvim'
-  use_local 'nvim-hook.lua'
-
-  use_local 'fennel.vim'
-  use_local 'rhai.vim'
-  use_local 'systemd.nvim'
-  use_local 'vim-nftables'
 end)
