34868d31f16418695f7bda31053435224b25584c
KokeKiwi <kokakiwi+git@kokakiwi.net>
2023-01-02 23:02:47 +0100

    Stuff

--- init.lua
+++ init.lua
@@ -1,8 +1,3 @@
-if os.getenv('NVIM_DEBUG') ~= '1' then
-  local ok, impatient = pcall(require, 'impatient')
-  if ok then impatient.enable_profile() end
-end
-
 -- Set a base colorscheme so that in case something goes wrong
 -- neovim won't destroy my eyes with its default colorscheme.
 vim.cmd.colorscheme('uwu')
