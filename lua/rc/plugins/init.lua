2fab71c0094d1651f2e005e2470d8876ce1cdfb3
KokeKiwi <kokakiwi+git@kokakiwi.net>
2023-01-16 15:40:40 +0100

    lazy.nvim: Disable checker

--- lua/rc/plugins/init.lua
+++ lua/rc/plugins/init.lua
@@ -4,7 +4,6 @@ local util = require('rc.plugins.util')
 local lazy = util.require_lazy()
 lazy.setup('rc.plugins.spec', {
     checker = {
-      enabled = true,
       notify = false,
     },
     dev = {
