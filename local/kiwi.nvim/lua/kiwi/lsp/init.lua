79eb3ad0fd68c78c773d83625d0f93dfda55e9c6
KokaKiwi <kokakiwi+dot@kokakiwi.net>
2023-05-05 00:22:06 +0200

    nvim: Auto-format go files

--- local/kiwi.nvim/lua/kiwi/lsp/init.lua
+++ local/kiwi.nvim/lua/kiwi/lsp/init.lua
@@ -1,7 +1,7 @@
 ---@diagnostic disable: unused-local
 local lsp = {}
 
-local AUTO_FORMAT_PATTERNS = { '*.rs', '*.ex', '*.exs', '*.hs', '*.toml' }
+local AUTO_FORMAT_PATTERNS = { '*.rs', '*.ex', '*.exs', '*.hs', '*.toml', '*.go' }
 
 function lsp.setup()
   require('mason').setup {}
