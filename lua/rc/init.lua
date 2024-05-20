6ea44f0f1768ed5fb3191f64f68569a9c2b1baa8
KokaKiwi <kokakiwi+git@kokakiwi.net>
2024-05-20 22:47:02 +0200

    rc: Remove providers config (handled by Nix)

--- lua/rc/init.lua
+++ lua/rc/init.lua
@@ -30,13 +30,6 @@ vim.o.timeoutlen = 500
 
 vim.o.exrc = true
 
-vim.g.python3_host_prog = '/usr/bin/python'
-
-vim.g.loaded_node_provider = 0
-vim.g.loaded_perl_provider = 0
-vim.g.loaded_python_provider = 0
-vim.g.loaded_ruby_provider = 0
-
 require_prefix('rc.init') {
   'format', 'input', 'ui',
   'keymaps',
